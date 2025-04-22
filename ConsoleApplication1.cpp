3187ddb6f28043e854fbe69cfed9e0a155a8a2a9
Olekso-alpaka <olesya.zhuravleva154@mail.ru>
2025-04-22 17:44:08 +0300

    Pelmeni

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t, M, qp, qf, qt;
+    double af;
+    cout << "Введите продолжительность смены: ";
+    if (!cin >> t && t <= 0)
+    {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Введите кол-во пельменей изготовленных за смену: ";
+    if (!cin >> M && M <= 0)
+    {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Введите сколько машина производит пельменей за рабочий период: ";
+    if (!cin >> qp && qp <= 0)
+    {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Введите сколько машина производит фарша за рабочий период: ";
+    if (!cin >> qf && qf <= 0)
+    {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Введите сколько машина производит теста за рабочий период: ";
+    if (!cin >> qt && qt <= 0)
+    {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Введите процент содержания фарша: ";
+    if (!cin >> af && af <= 0)
+    {
+        cout << "ERROR";
+        return 0;
+    }
+}
